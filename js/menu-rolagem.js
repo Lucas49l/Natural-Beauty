9c14242fb8604ad489d11166d66fc841939991c6
Lucas49l <lucasguimaraes1649@gmail.com>
2025-04-25 17:20:10 -0300

    Arumando Menu

--- js/menu-rolagem.js
+++ js/menu-rolagem.js
@@ -2,3 +2,16 @@ window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
     header.classList.toggle('rolagem',window.scrollY >0)
 })
+
+/*Menu Oculto*/
+
+const header = document.getElementById("header");
+const fundo = document.querySelector(".fundo");
+
+fundo.addEventListener("mouseenter", ()  => {
+    header.style.opacity="0";
+});
+
+fundo.addEventListener("mouseleave", () => {
+    header.style.opacity="1";
+});
